8730d1c5b063b00b39a5f43a6e4880cbfd65e196
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2020-09-04 16:21:04 -0500

    AWS Fargate

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # FROM redis
 # EXPOSE 6379
-# RUN mkdir /opt/redis && chown redis:redis /opt/redis
-# WORKDIR /opt/redis
+# # RUN mkdir /opt/redis && chown redis:redis /opt/redis
+# # WORKDIR /opt/redis
 
 FROM openjdk:8-jdk-alpine
 COPY "./target/demo-5.2.8.RELEASE.jar" "app.jar"
--- src/main/java/com/onboard/demo/config/RedisConfiguration.java
+++ src/main/java/com/onboard/demo/config/RedisConfiguration.java
@@ -3,6 +3,8 @@ package com.onboard.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -15,7 +17,16 @@ import com.onboard.demo.model.Student;
 public class RedisConfiguration {
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-		return new JedisConnectionFactory();
+		//return new JedisConnectionFactory();
+		/*JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName("http://3.92.28.19:6379");
+        //jedisConnectionFactory.setPort(6379);
+        jedisConnectionFactory.afterPropertiesSet();
+        return jedisConnectionFactory;*/
+        
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("3.92.28.19", 6379);
+        //redisStandaloneConfiguration.setPassword(RedisPassword.of("yourRedisPasswordIfAny"));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
 	}
 	
 	@Bean
